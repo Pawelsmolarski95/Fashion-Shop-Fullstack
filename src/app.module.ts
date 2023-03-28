8c92da545f4a323007c15a263aa415dd4a758009
Pawel Smolarski <smolenc1@gmail.com>
2023-03-28 19:05:29 +0200

    add users, products, auth, orders m/c/s

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsController } from './products/products.controller';
+import { ProductsModule } from './products/products.module';
+import { OrdersModule } from './orders/orders.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [ProductsModule, OrdersModule, UsersModule, AuthModule],
+  controllers: [AppController, ProductsController],
   providers: [AppService],
 })
 export class AppModule {}

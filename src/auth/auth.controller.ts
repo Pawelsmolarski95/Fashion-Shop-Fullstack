5f8f3683a8ce7e993552c061e07204599f777604
Pawel Smolarski <smolenc1@gmail.com>
2023-03-30 16:06:18 +0200

    add login to User

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,21 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDTO } from './dto/register.dto';
+import { LocalAuthGuard } from './local-auth.guard';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService) {}
 
-    @Post('/register') 
-    registerUser(@Body() registerUser: RegisterDTO) {
-        return this.authService.register(registerUser)
-    }
+  @UseGuards(LocalAuthGuard)
+  @Post('/register')
+  registerUser(@Body() registerUser: RegisterDTO) {
+    return this.authService.register(registerUser);
+  }
+
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  async login(@Request() req) {
+    return req.user;
+  }
 }

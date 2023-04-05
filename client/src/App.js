a57b8b27e2707ab0384da4c14ad4241cc6f7d4a9
Pawel Smolarski <smolenc1@gmail.com>
2023-04-05 07:30:22 +0200

    add ShoppingCart

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import { Route, Routes } from 'react-router';
 import './App.css';
 
-
 import NavBar from './components/layout/NavBar/NavBar';
 import Home from './components/pages/Home/Home';
 import SingleProduct from './components/pages/SingleProduct/SingleProduct';
@@ -9,6 +8,8 @@ import Footer from './components/layout/Footer/Footer';
 import MainProducts from './components/pages/MainProducts/MainProducts';
 import Login from './components/pages/Login/Login';
 import Register from './components/pages/Register/Register';
+import ShoppingCart from './components/pages/ShoppingCart/ShoppingCart';
+
 
 function App() {
   return (
@@ -17,11 +18,12 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<MainProducts />} />
-        <Route path="/product/:id" element={<SingleProduct/>} />
-        <Route path="/login" element={<Login/>} />
-        <Route path="/register" element={<Register/>} />
+        <Route path="/product/:id" element={<SingleProduct />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/shoppingcart" element={<ShoppingCart />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </main>
   );
 }

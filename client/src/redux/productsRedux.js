617fcc0bcd638e8491830da8acd016204abd7fbd
Pawel Smolarski <smolenc1@gmail.com>
2023-04-09 10:49:06 +0200

    add redux, create store and combineredcers

---
+++ client/src/redux/productsRedux.js
@@ -0,0 +1,40 @@
+import axios from 'axios';
+import { API_URL } from '../config';
+
+// selectors
+export const getAllProducts = ({ products }) => products.data;
+export const getProductById = ({ products }, productId) =>
+  products.data.find((product) => product.id === productId);
+export const getRequest = ({ products }) => products.request;
+
+// action name creator
+const reducerName = 'products';
+const createActionName = (actionName) => `app/${reducerName}/${actionName}`;
+
+const START_REQUEST = createActionName('START_REQUEST');
+const END_REQUEST = createActionName('END_REQUEST');
+const ERROR_REQUEST = createActionName('ERROR_REQUEST');
+
+const LOAD_PRODUCTS = createActionName('LOAD_PRODUCTS');
+
+// actions
+export const startRequest = (payload) => ({ payload, type: START_REQUEST });
+export const endRequest = (payload) => ({ payload, type: END_REQUEST });
+export const errorRequest = (payload) => ({ payload, type: ERROR_REQUEST });
+
+export const loadProducts = (payload) => ({ payload, type: LOAD_PRODUCTS });
+
+//thunk
+
+export const loadProductsRequest = () => {
+  return async (dispatch) => {
+    dispatch(startRequest({ name: 'LOAD_PRODUCTS' }));
+    try {
+      let res = await axios.get(`${API_URL}/products`);
+      dispatch(loadProducts(res.data));
+      dispatch(endRequest({ name: 'LOAD_PRODUCTS' }));
+    } catch (err) {
+      dispatch(errorRequest({ name: 'LOAD_PRODUCTS', error: err.message }));
+    }
+  };
+};

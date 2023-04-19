471835cf2c8728e6c77fc819f8e18c09bd6d6e2e
Pawel Smolarski <smolenc1@gmail.com>
2023-04-19 14:42:28 +0200

    WIP deploy

--- client/src/config.js
+++ client/src/config.js
@@ -1,8 +1,21 @@
 
-export const IMGS_URL =
-'https://fashion-e-commerce.onrender.com/public';
+// export const IMGS_URL =
+// 'https://fashion-e-commerce.onrender.com/public';
 
 
-    export const API_URL =
-   'https://fashion-e-commerce.onrender.com/api';
+//     export const API_URL =
+//    'https://fashion-e-commerce.onrender.com/api';
 
+
+// export const IMGS_URL =
+// 'http://localhost:3000/public';
+
+
+//     export const API_URL =
+//   process.env.NODE_ENV === 'production' ? '' : 'http://localhost:3000/api';
+
+  export const IMGS_URL =
+  process.env.NODE_ENV === 'production' ? '/public' : 'http://localhost:3000/public';
+
+export const API_URL =
+  process.env.NODE_ENV === 'production' ? 'https://fashion-e-commerce.onrender.com' : 'http://localhost:3000/api';

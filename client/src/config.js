dce24e5f555e41ae35937c4166c31c946217d6ff
Pawel Smolarski <smolenc1@gmail.com>
2023-05-16 13:02:54 +0200

    WIP filters

--- client/src/config.js
+++ client/src/config.js
@@ -1,19 +1,4 @@
 
-// export const IMGS_URL =
-// 'https://fashion-e-commerce.onrender.com/public';
-
-
-//     export const API_URL =
-//    'https://fashion-e-commerce.onrender.com/api';
-
-
-// export const IMGS_URL =
-// 'http://localhost:3000/public';
-
-
-//     export const API_URL =
-//   process.env.NODE_ENV === 'production' ? '' : 'http://localhost:3000/api';
-
   export const IMGS_URL =
   process.env.NODE_ENV === 'production' ? '/public' : 'http://localhost:3000/public';
 
